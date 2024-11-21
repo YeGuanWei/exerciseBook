609868aba95fd0483dfb647030f728b69d2b4591
YeGuanWei <787139598@qq.com>
2024-11-21 11:28:41 +0800

    初始化项目

--- src/main/java/com/hesc/ExerciseBookApplication.java
+++ src/main/java/com/hesc/ExerciseBookApplication.java
@@ -1,25 +1,15 @@
 package com.hesc;
 
-import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ServletComponentScan
-@EnableScheduling
-@EnableSchedulerLock(defaultLockAtMostFor = "PT5M")//默认分布式锁的时间
-@EnableSwagger2
-@EnableAsync
-@MapperScan({"com.hesc.mapper", "com.hesc.model.obj.mapper"})
 public class ExerciseBookApplication extends SpringBootServletInitializer {
 
     private static final Logger log = LogManager.getLogger(ExerciseBookApplication.class);
--- src/main/java/com/hesc/constant/SqlConstant.java
+++
@@ -1,7 +0,0 @@
-package com.hesc.constant;
-
-public class SqlConstant {
-
-    public static final String LIMIT_ONE = "limit 1";
-
-}
